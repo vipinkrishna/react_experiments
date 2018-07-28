85ddaf5eb049ac95f582610d1ec4341a35ae125c
vipinkrishna <vipinkrishna.in@gmail.com>
2018-07-28 22:51:11 +0530

    TodoList, Flip-Move Animation

---
+++ todolist/README.md
@@ -0,0 +1,5 @@
+# REACT TODOLIST
+
+
+https://github.com/vipinkrishna (Whatsapp: +917907450532)
+vipinkrishna.in@gmail.com
---
+++ todolist/src/TodoItems.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import FlipMove from 'react-flip-move';
+
+class TodoItems extends Component {
+
+    constructor(props) {
+        super(props);
+        this.createTasks = this.createTasks.bind(this);
+    }
+
+    createTasks(item) {
+        return <li onClick={() => this.props.delete(item.key)} key={item.key}>{item.text}</li>
+    }
+
+    render() {
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map(this.createTasks);
+        return(
+            <ul className="theList">
+            <FlipMove duration={250} easing='ease-out'>
+                {listItems}
+            </FlipMove>
+            </ul>
+        );
+    }
+}
+
+export default TodoItems;
